fdd6b2572916ddca16ec4e685d5b6554f3a8d108
Parker <parkerproject@gmail.com>
2016-02-28 16:58:47 -0500

    updates

--- server/controllers/paid.js
+++ server/controllers/paid.js
@@ -1,15 +1,32 @@
 'use strict'
 require('dotenv').load()
-var collections = ['paid']
+var collections = ['merchants']
 var db = require('mongojs').connect(process.env.DEALSBOX_MONGODB_URL, collections)
 
 module.exports = {
     index: {
         handler: function (request, reply) {
             let data = request.payload
-            db.paid.save(data, function () {
-                reply('save')
+            let email = data.data.receipt_email
+            let created_date = data.data.created
+            created_date = new Date(created_date * 1000)
+            created_date = created_date.toISOString()
+
+            db.merchants.findAndModify({
+                query: {
+                    business_email: email
+                },
+                update: {
+                    $set: {
+                        current_period_end: created_date
+                    }
+                },
+                new: false
+            }, function (err, doc, lastErrorObject) {
+                if (err) console.log(err)
+                reply('updated')
             })
+
         }
     }
 }
