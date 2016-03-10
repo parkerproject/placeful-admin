eac28026c9bb416c5739f9d44a40123f0626600b
Parker <parkerproject@gmail.com>
2016-03-09 20:21:43 -0500

    fix issues

--- server/controllers/refresh_deals.js
+++ server/controllers/refresh_deals.js
@@ -10,6 +10,18 @@ const typeform_url = 'https://api.typeform.com/v0/form/' + process.env.FORM_ID +
 const slug = require('slug')
 const mersenne = require('mersenne')
 
+function tConvert(time) {
+  // Check correct time format and split into components
+  time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time]
+
+  if (time.length > 1) { // If time format correct
+    time = time.slice(1) // Remove full string match value
+    time[5] = +time[0] < 12 ? 'AM' : 'PM' // Set AM/PM
+    time[0] = +time[0] % 12 || 12 // Adjust hours
+  }
+  return time.join('') // return adjusted time or original string
+}
+
 module.exports = {
   index: {
     handler: function (request, reply) {
@@ -23,14 +35,23 @@ module.exports = {
         if (!error && response.statusCode === 200) {
           let results = JSON.parse(body).responses
 
-          console.log(results)
-
           if (results.length !== 0) {
             let currentPromotion = _.filter(results, function (result) {
               return result.hidden.promotion_id === promotion_id
             })
 
             currentPromotion = currentPromotion[0]
+
+            // currentPromotion.answers.number_18667382 - start hour
+            // currentPromotion.answers.number_18667408 - start min
+            // currentPromotion.answers.number_18667562 - end hour
+            // currentPromotion.answers.number_18667563  end min
+
+            let start_time = currentPromotion.answers.number_18667382 + ':' + currentPromotion.answers.number_18667408
+            start_time = tConvert(start_time)
+
+            let end_time = currentPromotion.answers.number_18667562 + ':' + currentPromotion.answers.number_18667563
+            end_time = tConvert(end_time)
 
             promotion.merchant_id = currentPromotion.hidden.business_id
             promotion.merchant_locality = currentPromotion.hidden.merchant_locality
@@ -51,6 +72,8 @@ module.exports = {
             promotion.slug = slug(currentPromotion.answers.textfield_17217315)
             promotion.ticket_id = mersenne.rand()
             promotion.likes = []
+            promotion.start_time = start_time
+            promotion.end_time = end_time
 
             let props = Object.keys(currentPromotion.answers)
             let tags = []
