bf26abf4763b2b1bda25357bc52494c13b763d50
Parker <parkerproject@gmail.com>
2016-03-13 19:20:58 -0400

    updated email

--- server/controllers/refresh_deals.js
+++ server/controllers/refresh_deals.js
@@ -86,9 +86,9 @@ module.exports = {
             db.promotions.save(promotion, function () {
               uploader(promotion.large_image, promotion_id)
 
-              let content = `A new promotion <a href="http://placeful.co/promotion/${promotion_id}/${promotion.slug}">${promotion.title}</a> has been created
+              let content = `A new promotion <a href="http://placeful.co/promotion/${promotion_id}/${promotion.slug}">${promotion.title}</a> has been created!
               <p>if you like what you see, go ahead and approve in the admin</p>
-              Thanks<br />
+              Thanks,<br />
               Placeful robot`
 
               sendEmail(process.env.ADMIN_EMAIL, 'New promotion', content)
