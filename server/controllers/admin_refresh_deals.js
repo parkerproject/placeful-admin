e484c50fe8e905f5c2d31b8023748c6c3fde8580
Parker <parkerproject@gmail.com>
2016-03-18 18:18:03 -0400

    updates

--- server/controllers/admin_refresh_deals.js
+++ server/controllers/admin_refresh_deals.js
@@ -90,7 +90,7 @@ module.exports = {
                 db.promotions.save(promotion, function () {
                   uploader(promotion.large_image, promotion_id)
 
-                  promotion.business_email = merchantAccount.business_id + '@placeful.co'
+                  promotion.business_email = promotion.merchant_id + '@placeful.co'
                   promotion.password = null
                   promotion.icon = null
                   promotion.website = null
