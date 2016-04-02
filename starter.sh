7508bbea5a9ca87c49d67e92c863c2245dfa77c1
Parker <parkerproject@gmail.com>
2016-04-02 11:34:10 -0400

    updated startup script

--- starter.sh
+++ starter.sh
@@ -4,5 +4,5 @@ if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 
 then
   export PATH=/usr/local/bin:$PATH
   #export NODE_ENV=production
-  cd /var/www/placeful-merchant && pm2 start server.js -i 2 --name="placeful" >> forever.log 2>&1
+  cd /var/www/placeful-merchant && pm2 start server.js --name="merchant" -i 1 >> forever.log 2>&1
 fi
