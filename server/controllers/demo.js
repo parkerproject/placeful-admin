bab41901509fcd62999c29de92de38857174abd9
Parker <parkerproject@gmail.com>
2016-04-12 19:31:52 -0400

    added cron for deleting demo

---
+++ server/controllers/demo.js
@@ -0,0 +1,18 @@
+'use strict'
+require('dotenv').load()
+const collections = ['promotions']
+const db = require('mongojs').connect(process.env.DEALSBOX_MONGODB_URL, collections)
+const schedule = require('node-schedule')
+const sendEmail = require('./send_email')
+
+function end() {
+  let content = 'Demo data deleted'
+  sendEmail(process.env.DEV_EMAIL, 'Demo data refreshed', content)
+}
+let j = schedule.scheduleJob('0 10 * * *', function () {
+  db.promotions.remove({
+    merchant_id: 'pcCxqeV5C5O6OtpEqMhw'
+  }, () => {
+    end()
+  })
+})
