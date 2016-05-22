c2cd574c1c9ac27b4f88ed5b112eb11b6006aee3
Parker <parkerproject@gmail.com>
2016-05-22 11:16:56 -0400

    update the demo

--- server/controllers/demo.js
+++ server/controllers/demo.js
@@ -5,14 +5,14 @@ const db = require('mongojs').connect(process.env.DEALSBOX_MONGODB_URL, collecti
 const schedule = require('node-schedule')
 const sendEmail = require('./send_email')
 
-function end() {
+function end () {
   let content = 'Demo data deleted'
   sendEmail(process.env.DEV_EMAIL, 'Demo data refreshed', content)
 }
-let j = schedule.scheduleJob('0 10 * * *', function () {
-  db.promotions.remove({
-    merchant_id: 'pcCxqeV5C5O6OtpEqMhw'
-  }, () => {
-    end()
-  })
-})
+// let j = schedule.scheduleJob('0 10 * * *', function () {
+//   db.promotions.remove({
+//     merchant_id: 'pcCxqeV5C5O6OtpEqMhw'
+//   }, () => {
+//     end()
+//   })
+// })
